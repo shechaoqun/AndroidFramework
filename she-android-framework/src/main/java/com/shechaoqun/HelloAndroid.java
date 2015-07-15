150ab097a78564d2a2a7048628421286b2069ad6
shechaoqun <shechaoqun@gmail.com>
2015-07-15 11:15:56 +0800

    首发
    首发

---
+++ she-android-framework/src/main/java/com/shechaoqun/HelloAndroid.java
@@ -0,0 +1,12 @@
+package com.shechaoqun;
+
+import com.shechaoqun.log.LogManager;
+
+/**
+ * Created by chaoqun on 2015/7/9.
+ */
+public class HelloAndroid {
+    public void A() {
+      //  LogManager.getLogger().
+    }
+}
---
+++ she-android-framework/src/main/java/com/shechaoqun/log/impl/LogPrintToLogCat.java
@@ -0,0 +1,9 @@
+package com.shechaoqun.log.impl;
+
+/**
+ * 日志类，将日志打印到logcat上
+ * Created by chaoqun on 2015/7/9
+ * @since 1.0
+ */
+public class LogPrintToLogCat {
+}
